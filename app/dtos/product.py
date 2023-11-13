04e3863dad0291267214ccc1beeae69b3ed09a41
Ranier Dias <110170807+RanierDias@users.noreply.github.com>
2023-11-13 11:48:09 -0300

    Update dtos to products

--- app/dtos/product.py
+++ app/dtos/product.py
@@ -5,13 +5,14 @@ class VariationsProductSchema(Schema):
     image = fields.String(required=True)
     value = fields.String(required=True)
     price = fields.Float(required=True)
+    unit = fields.String()
 
 
 class ProductSchema(Schema):
     _id = fields.String(dump_only=True)
+    code = fields.String()
     name = fields.String(required=True)
     description = fields.String()
     category = fields.String(required=True)
-    sale_type = fields.String()
     variations = fields.Nested(
         VariationsProductSchema, many=True, required=True)
